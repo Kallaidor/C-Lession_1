89631c65f34b2b72377127621bc7b20823bf83b8
Kallaidor <signalup3000@yandex.ru>
2022-06-05 21:51:05 +0300

    add_random

--- Example003_sum/Program.cs
+++ Example003_sum/Program.cs
@@ -1,4 +1,10 @@
-﻿int numberA = 5;
-int numberB = 8;
-int Result= numberA+numberB;
+﻿int numberA = new Random().Next(1,10); //1 2 3 4...9
+int numberB = new Random().Next(1,10); //1 2 3 4...9
+
+int Result = numberA+numberB;
+Console.Write("Число A = ");
+Console.WriteLine(numberA);
+Console.Write("Число B = ");
+Console.WriteLine(numberB);
+Console.Write("Сумма = ");
 Console.WriteLine(Result);
