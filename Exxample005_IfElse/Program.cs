7a2a2a7d42ab51cbb10ea98580ef9e5fd815c471
Kallaidor <signalup3000@yandex.ru>
2022-06-06 19:55:26 +0300

    add_005_if_else

---
+++ Exxample005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Привет, как тебя зовут?");
+string username = Console.ReadLine();
+
+if(username == "Маша");
+{
+Console.WriteLine("Ура! это же маша");
+}
+else
+{
+Console.Write("Привет,");
+Console.Write(username);
+}
